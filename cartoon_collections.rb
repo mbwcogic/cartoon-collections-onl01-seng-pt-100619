1e3ffa4de0cb42209a0605804748745f9b5c27d6
Michael Waldo <mbwcogic@yahoo.com>
2019-10-22 14:34:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(array)# code an argument here
  array_new do
   
-  array.find
+  array.find ||
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
